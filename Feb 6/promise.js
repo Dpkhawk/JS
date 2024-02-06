1e29e0c197c0d9ad47bf0e1fe0ce12064e59cb45
Dpkhawk <deepak.periyasamy@aspiresys.com>
2024-02-06 14:38:02 +0530

    06/02/2024

---
+++ Feb 6/promise.js
@@ -0,0 +1,74 @@
+
+// const promise1=new Promise((resolve,reject)=>
+
+// {
+
+//     setTimeout(()=>
+
+//     {
+
+//         resolve(10)
+
+//     },5000)
+
+// })
+// .then(data=>console.log(data))
+
+// const promise2=new Promise((resolve,reject)=>
+
+// {
+
+//     setTimeout(()=>
+
+//     {
+
+//         resolve(20)
+
+//     },3000)
+
+// })
+
+// console.log(promise1+promise2);
+
+// Promise.all([promise1,promise2]).then((values)=>
+
+// {
+
+//     console.log(values);
+
+//     const val1=values[0];
+
+//     const val2=values[1];
+
+//     const sum=val1+val2;
+
+//     console.log(sum);
+
+// });
+
+// const details={
+//     name:"deepak",
+//     age:21
+// }
+// for(let [keys,values] in Object.entries(details)){
+//     console.log(keys,values);
+// }
+// for(let keys in details){
+//     console.log(keys,details[keys]);
+// }
+// const arr=[1,2,3,4]
+// for(let k in arr){
+//     console.log(arr[k]);
+// }
+
+class person{
+    constructor(name){
+        this.name=name;
+        // this.age=age;
+    }
+    greet=()=>{
+       console.log(`${this.name}`);
+    }
+}
+const obj1=new person("deepak");
+obj1.greet();
